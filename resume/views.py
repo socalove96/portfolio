8f1ca03f6bef06144f6e2ec0d2eebc3974c0bfec
Alex <alexmalek.csm@gmail.com>
2021-06-19 16:39:50 -0400

    first commit

---
+++ resume/views.py
@@ -0,0 +1,11 @@
+from typing import ContextManager
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context = {
+
+    }
+    return render(request, 'index.html', context=context)
+
